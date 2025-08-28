93b242cc48b7557116322ddfed35815308d51110
heygeeji <heygeeji@gmail.com>
2025-08-28 12:15:36 +0900

    Model을 통해서 컨트롤러의 데이터를 html에 전달. html에서는 th:each와 th:text를 이용해 제어

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -2,8 +2,10 @@ package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
@@ -14,14 +16,18 @@ public class QuestionController {
 
     @GetMapping("/question/list")
 //    @ResponseBody
-    public String list() {
+    public String list(Model model) {
 
         String questions = questionRepository.findAll()
                 .stream()
                 .map(e -> "<li)%s</li>".formatted(e.getSubject()))
                 .collect(Collectors.joining("\n"));
 
-        System.out.println(questionRepository.findAll().size());
+        String value = "홍길동";
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
+
+        model.addAttribute("value", value);  //map.put과 같음
+        model.addAttribute("list", list);
 
 
         return "question_list";
