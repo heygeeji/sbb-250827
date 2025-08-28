e3fe8a10392fceb501d75170668a3b6dfca5b4ea
heygeeji <heygeeji@gmail.com>
2025-08-28 11:37:51 +0900

    질문 목록 구현. 컨트롤러에서 직접 html을 작성 불편

---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,47 @@
+package com.mysite.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.stream.Collectors;
+
+@Controller
+@RequiredArgsConstructor
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/question/list")
+    @ResponseBody
+    public String list() {
+
+        String questions = questionRepository.findAll()
+                .stream()
+                .map(e -> "<li)%s</li>".formatted(e.getSubject()))
+                .collect(Collectors.joining("\n"));
+
+        return """
+                <html>
+                    <body>
+                        <ul>
+                           %s
+                        </ul>
+                    </body>
+                </html>
+                """.formatted(questions);
+
+//        return """
+//                <html>
+//                    <body>
+//                        <ul>
+//                            <li>질문1</li>
+//                            <li>질문2</li>
+//                            <li>질문3</li>
+//                        </ul>
+//                    </body>
+//                </html>
+//                """;
+    }
+}
